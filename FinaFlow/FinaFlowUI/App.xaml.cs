9bbac9071489152bae9e512d7eb79e81d53c8830
mattsfowler <mattsfwlr@gmail.com>
2021-10-28 21:23:32 +0100

    Project Repoint

--- FinaFlow/FinaFlowUI/App.xaml.cs
+++ FinaFlow/FinaFlowUI/App.xaml.cs
@@ -31,7 +31,6 @@ namespace FinaFlowUI
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
-            SettingsHelper.Initialise();
         }
 
         /// <summary>
